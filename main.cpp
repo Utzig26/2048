3d41aded2fe7068ca94ccc17f53a8f83ccf1c378
mariferth <69264130+mariferth@users.noreply.github.com>
2020-08-21 18:01:22 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <windows.h>
 #include <conio.h>
+#include "Pilha_LE.h" //Biblioteca que contém as funções referentes a pilha.
+
 
 //Criei esse main só para testar as funções criadas.
 int main() {
@@ -15,17 +17,18 @@ int main() {
     int recorde = 0; //Falta criar função para armazenar a melhor pontuação!
     char tecla1[2];
     int tecla;
+    no *L;
     inicializa_tabuleiro(tab, &pont);
     do {
         imprime_tabuleiro (tab, recorde, &pont);
         gets(tecla1);
         tecla = atoi(tecla1);
-        movimentos(tab, tecla, &pont);
-        if (movimentos(tab,tecla, &pont) == 1) //Se ocorrer movimento dos blocos, um número aleatório é adicionado ao tabuleiro logo após.
+        movimentos(tab, tecla, &pont, &L);
+        if (movimentos(tab,tecla, &pont, &L) == 1) 
             num_aleatorio(tab);
+        atualiza_soma(tab, tecla, &L);
         //system("cls");
         imprime_tabuleiro (tab, recorde, &pont);
-    } while ((tecla != 78) && (perde(tab) != 1)); //Enquanto não for digitado o número 78 (era pra ser a tecla N) ou ainda houver espaços no tabuleiro o loop continua.
+    } while ((tecla != 78) && (perde(tab) != 1)); 
     return 0;
 }
-
